b6b4f25c560f722cdaee9fc83620dcc9c2f15d9a
Denis <den.zhmakin.00@gmail.com>
2019-10-01 13:41:26 +0300

    Added datagrid

---
+++ DataGrid/DataGrid/Items.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid
+{
+    class Items
+    {
+        public Items(int id, string family, int point)
+        {
+            Id = id;
+            Family = family;
+            Point = point;
+        }
+
+        public int Id { get; set; }
+        public string Family { get; set; }
+        public int Point { get; set; }
+    }
+}
---
+++ DataGrid/DataGrid/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGrid
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Items[] result = new Items[45];
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Dtgrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = (new Items(i + 1, People.family_array[i],People.point_array[i]));
+            }
+
+            dtgrid.ItemsSource = result;
+        }
+    }
+}
